f096a8164f0413f9ec05ab25d363eaffd8926250
jayalalitha027 <130637497+jayalalitha027@users.noreply.github.com>
2023-04-15 17:31:26 +0530

    Update views.py

--- FirstProject/FirstApp/views.py
+++ FirstProject/FirstApp/views.py
@@ -164,13 +164,4 @@ def demo(request):
 		</center>''';
 	return HttpResponse(htmldata);
     
-    
-def homepage(request):
-    htmldata='''<center>
-        <h1>Welcome to DEFAULT Home-Page!!!</h1>
-        <hr />
-        <h2>Your Request Page is Not-Found...</h2>
-        <hr />
-        <h3>Plz try other URL or Links!!!</h3>
-    </center>''';
-    return HttpResponse(htmldata);
+
